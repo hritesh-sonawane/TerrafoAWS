ab6d8bb9ccdc3359a6653d3c30660862797f31f3
hritesh93 <hr7tesh@gmail.com>
2022-02-04 10:53:59 +0530

    🌉 provide an igw resource

--- main.tf
+++ main.tf
@@ -18,3 +18,11 @@ resource "aws_subnet" "tf_public_subnet" {
     Name = "dev-public"
   }
 }
+
+resource "aws_internet_gateway" "tf_igw" {
+  vpc_id = aws_vpc.tf_vpc.id
+
+  tags = {
+    Name = "dev-igw"
+  }
+}
